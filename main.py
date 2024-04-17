8f902c7cb54aa33b6e89a6fe38d001e26f469093
Cayo Cesar <96149641+Cayo-Cesar@users.noreply.github.com>
2024-04-17 08:17:09 -0300

    Atualizar implementação do algoritmo de Smith-Waterman em main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Implementação Smith-Waterman
 
-seq1 = "ACGTT"
-seq2 = "ACCTT"
+seq1 = "AGTACCA" #Vertical
+seq2 = "CATTCC" #Horizontal
 
 match = 1
 mismatch = -1
@@ -24,6 +24,19 @@ for j in range(1, cols):
 
 for i in range(1, rows):
     score_matrix[i][0] = gap_penalty * i
+
+for i in range(1, rows):
+    for j in range(1, cols):
+        if seq1[i - 1] == seq2[j - 1]:
+            score = match
+        else:
+            score = mismatch
+
+        right = score_matrix[i][j - 1] + gap_penalty
+        down = score_matrix[i - 1][j] + gap_penalty
+        diag = score_matrix[i - 1][j - 1] + score
+
+        score_matrix[i][j] = max(right, down, diag, 0)
 
 for row in score_matrix:
     print(row)
