fca5b403cfcb0afb9fc734f2281005f0dd327354
Cayo Cesar <96149641+Cayo-Cesar@users.noreply.github.com>
2024-04-17 09:22:32 -0300

    Corrigir inicialização da matriz de pontuações em main.py

--- main.py
+++ main.py
@@ -1,33 +1,24 @@
-#Implementação Smith-Waterman
-
-seq1 = "AGTACCA" #Vertical
-seq2 = "CATTCC" #Horizontal
+seq1 = "AGTACCA" # Vertical
+seq2 = "CATTCC"  # Horizontal
 
 match = 1
 mismatch = -1
 gap_penalty = -2
 
-rows = len(seq1) + 1
-cols = len(seq2) + 1
+rows = len(seq1) + 2
+cols = len(seq2) + 2
 
-j = 0
-i = 0
+score_matrix = [[0 for _ in range(cols)] for _ in range(rows)]
 
-right = None
-down = None
-diag = None
+for i in range(2, rows):
+    score_matrix[i][0] = gap_penalty * (i - 1)
 
-score_matrix = [[0 for j in range(cols)] for i in range(rows)]
-
-for j in range(1, cols):
-    score_matrix[0][j] = gap_penalty * j
-
-for i in range(1, rows):
-    score_matrix[i][0] = gap_penalty * i
+for j in range(2, cols):
+    score_matrix[0][j] = gap_penalty * (j - 1)
 
 for i in range(1, rows):
     for j in range(1, cols):
-        if seq1[i - 1] == seq2[j - 1]:
+        if seq1[i - 2] == seq2[j - 2]:  
             score = match
         else:
             score = mismatch
@@ -40,4 +31,3 @@ for i in range(1, rows):
 
 for row in score_matrix:
     print(row)
-
