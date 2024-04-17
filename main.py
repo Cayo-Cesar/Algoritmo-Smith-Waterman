3e5b69dd1729f4e1eec01f47d71e15f58c21891d
Cayo Cesar <96149641+Cayo-Cesar@users.noreply.github.com>
2024-04-17 08:07:23 -0300

    Corrigir inicialização da matriz de pontuações em main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ match = 1
 mismatch = -1
 gap_penalty = -2
 
-# Inicialização da matriz de pontuações
 rows = len(seq1) + 1
 cols = len(seq2) + 1
 
@@ -16,15 +15,12 @@ i = 0
 
 score_matrix = [[0 for j in range(cols)] for i in range(rows)]
 
-# Inicialização da primeira linha com penalidades de gap
 for j in range(1, cols):
     score_matrix[0][j] = gap_penalty * j
 
-# Inicialização da primeira coluna com penalidades de gap
 for i in range(1, rows):
     score_matrix[i][0] = gap_penalty * i
 
-# Impressão da matriz
 for row in score_matrix:
     print(row)
 
