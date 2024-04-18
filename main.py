9c5ea0f7f794d177aad8222a9d7cc502efd065f1
Cayo Cesar <96149641+Cayo-Cesar@users.noreply.github.com>
2024-04-18 07:23:07 -0300

    Criação da função score_matrix

--- main.py
+++ main.py
@@ -2,6 +2,38 @@
 #Autor: Cayo Cesar
 #Data: 17/04/2024
 
+
+def score_matrix(seq1, seq2, match, mismatch, gap_penalty):
+    rows = len(seq1) + 1
+    cols = len(seq2) + 1
+
+    right = 0
+    down = 0
+    diag = 0
+
+    score_matrix = [[0 for _ in range(cols)] for _ in range(rows)]
+
+    for i in range(1, rows):
+        score_matrix[i][0] = gap_penalty * i
+
+    for j in range(1, cols):
+        score_matrix[0][j] = gap_penalty * j
+
+    for i in range(1, rows):
+        for j in range(1, cols):
+            if seq1[i - 1] == seq2[j - 1]:
+                score = match
+            else:
+                score = mismatch
+
+            right = score_matrix[i][j - 1] + gap_penalty
+            down = score_matrix[i - 1][j] + gap_penalty
+            diag = score_matrix[i - 1][j - 1] + score
+
+            score_matrix[i][j] = max(right, down, diag)
+
+    return score_matrix
+
 # Função para imprimir a matriz de score e salvar em um txt
 def print_matrix(matrix):
     with open('score_matrix.txt', 'w') as file:
@@ -21,38 +53,8 @@ match = 1
 mismatch = -1
 gap_penalty = -2
 
-# Inicialização das linhas e colunas da matriz de score
-rows = len(seq1) + 1 
-cols = len(seq2) + 1  
-
-# Inicialização do valor das variaveis de direção
-right = 0
-down = 0
-diag = 0
-
-# Inicialização da matriz de score
-score_matrix = [[0 for _ in range(cols)] for _ in range(rows)]
-
-# Inicialização dos valores de penalidade de gap para as linhas e colunas iniciais
-for i in range(1, rows):
-    score_matrix[i][0] = gap_penalty * i
-
-for j in range(1, cols):
-    score_matrix[0][j] = gap_penalty * j
-
-# Preenchimento da matriz de score
-for i in range(1, rows):
-    for j in range(1, cols):
-        if seq1[i - 1] == seq2[j - 1]:  
-            score = match
-        else:
-            score = mismatch
-
-        right = score_matrix[i][j - 1] + gap_penalty
-        down = score_matrix[i - 1][j] + gap_penalty
-        diag = score_matrix[i - 1][j - 1] + score
-
-        score_matrix[i][j] = max(right, down, diag)
+# Chamada da função score_matrix
+score_matrix = score_matrix(seq1, seq2, match, mismatch, gap_penalty)
 
 # Impressão da matriz de score
 print_matrix(score_matrix)
