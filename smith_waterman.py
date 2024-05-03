619fbfa3a875f5560d510917405b77660ef8e6ce
Cayo Cesar <cayocesar652@gmail.com>
2024-05-03 16:40:20 -0300

    Adição do backtracing utilizando pilha pra armazenar os movimentos

--- smith_waterman.py
+++ smith_waterman.py
@@ -63,19 +63,17 @@ def score_matrix(seq1, seq2, match, mismatch, gap_penalty):
     return score_matrix
 
 def backtracing(score_matrix, seq1, seq2, match, mismatch, gap_penalty):
-    # Inicialização de variáveis
+
     rows = len(seq1) + 1
     cols = len(seq2) + 1
 
-    # Inicialização de variáveis para percorrer a matriz
     i = rows - 1
     j = cols - 1
 
-    # Inicialização de strings para armazenar os alinhamentos
-    align1 = ''
-    align2 = ''
+    # Inicialização da pilha para armazenar os movimentos
+    stack = []
 
-    # Loop para percorrer a matriz de score
+    # Percorrer a matriz de scores
     while i > 0 and j > 0:
         # Cálculo do score atual e dos scores adjacentes
         score = score_matrix[i][j]
@@ -83,47 +81,48 @@ def backtracing(score_matrix, seq1, seq2, match, mismatch, gap_penalty):
         score_up = score_matrix[i][j - 1]
         score_left = score_matrix[i - 1][j]
 
-        '''
-        Se o score atual for igual ao score da diagonal mais o match ou mismatch, 
-        significa que o elemento veio da diagonal e vai ser adicionado na sequencia 
-        de alinhamento de ambas as sequências. Se o score atual for igual ao score
-        da esquerda mais o gap_penalty ou de cima mais o gap_penalty, significa que 
-        o elemento veio da esquerda ou de cima, nesse caso somente uma das sequências 
-        vai ser adicionada na sequência de alinhamento vai ser adicionada um gap.
-        '''
+        # Determina o movimento atual e adiciona à pilha
         if score == score_diag + (match if seq1[i - 1] == seq2[j - 1] else mismatch):
-            align1 += seq1[i - 1]
-            align2 += seq2[j - 1]
+            stack.append('D')  # Diagonal
             i -= 1
             j -= 1
         elif score == score_left + gap_penalty:
-            align1 += seq1[i - 1]
-            align2 += '-'
+            stack.append('U')  # Up
             i -= 1
         elif score == score_up + gap_penalty:
-            align1 += '-'
-            align2 += seq2[j - 1]
+            stack.append('L')  # Left
             j -= 1
 
-    # Se i ou j for maior que 0, significa que ainda existem elementos a serem adicionados
-    # nas sequências de alinhamento, nesse caso, adicionamos os elementos restantes e os gaps.
+    # Completa a pilha 
     while i > 0:
-        align1 += seq1[i - 1]
-        align2 += '-'
+        stack.append('U')
         i -= 1
-
     while j > 0:
-        align1 += '-'
-        align2 += seq2[j - 1]
+        stack.append('L')
         j -= 1
 
-    # Inversão das strings para obter o alinhamento correto
-    align1 = align1[::-1]
-    align2 = align2[::-1]
+    # Inversão da pilha
+    stack.reverse()
 
-    # Impressão dos alinhamentos
-    #print(align1)
-    #print(align2)
+    # Reconstrução dos alinhamentos a partir da pilha de movimentos
+    align1 = ''
+    align2 = ''
+    i = 0
+    j = 0
+    for move in stack:
+        if move == 'D':
+            align1 += seq1[i]
+            align2 += seq2[j]
+            i += 1
+            j += 1
+        elif move == 'U':
+            align1 += seq1[i]
+            align2 += '-'
+            i += 1
+        elif move == 'L':
+            align1 += '-'
+            align2 += seq2[j]
+            j += 1
 
     return align1, align2
 
