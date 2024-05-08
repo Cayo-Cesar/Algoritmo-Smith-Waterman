f85342f1dbf371a469a075dae5cf7dcd947ce3d2
Cayo Cesar <cayocesar652@gmail.com>
2024-05-07 21:35:48 -0300

    feat: Refatorar função score_matrix para melhorar desempenho

--- smith_waterman.py
+++ smith_waterman.py
@@ -15,26 +15,17 @@ gap_penalty
 # Função para criar a matriz de score
 def score_matrix(seq1, seq2, match, mismatch, gap_penalty):
 
-    # stack_movements = []
     rows = len(seq1) + 1
     cols = len(seq2) + 1
 
-    #Direção de Movimentos
-    right = 0
-    down = 0
-    diag = 0
-
-    #Inicializa a Matriz com 0 em todas as posições
     score_matrix = [[0 for _ in range(cols)] for _ in range(rows)]
 
-    #Incrementa a primeira linha e coluna com os valores de gap_penalty * i
     for i in range(1, rows):
         score_matrix[i][0] = gap_penalty * i
 
     for j in range(1, cols):
         score_matrix[0][j] = gap_penalty * j
 
-    #Preenche a matriz de score verificando se os elementos são iguais ou diferentes( match ou mismatch) e atribuindo o valor correto de acordo com a situação 
     for i in range(1, rows):
         for j in range(1, cols):
             if seq1[i - 1] == seq2[j - 1]:
@@ -42,24 +33,14 @@ def score_matrix(seq1, seq2, match, mismatch, gap_penalty):
             else:
                 score = mismatch
 
-    #Calcula os valores de right, down e diag e atribui o maior valor a posição atual da matriz
             right = score_matrix[i][j - 1] + gap_penalty
             down = score_matrix[i - 1][j] + gap_penalty
             diag = score_matrix[i - 1][j - 1] + score
 
-            # # Append na pilha de movimentos para a realização do backtracing posteriormente
-            # if max(right, down, diag) == diag:
-            #     stack_movements.append('D')
-
-            # elif max(right, down, diag) == down:
-            #     stack_movements.append('U')
-
-            # elif max(right, down, diag) == right:
-            #     stack_movements.append('L')
-
-    #Atribui o maior valor a posição atual da matriz
             score_matrix[i][j] = max(right, down, diag)
+
     return score_matrix
+
 
 def backtracing(score_matrix, seq1, seq2, match, mismatch, gap_penalty):
 
@@ -143,18 +124,18 @@ def print_matrix(matrix, seq1, seq2):
         # Imprime a matriz de score e a sequência 1 ja invertida e é feita a adição do valor U na primeira posição da matriz
         for i in range(rows):
             if i < rows - 1:
-                f.write(seq1_reversed[i] + "\t")
+                f.write(seq1_reversed[i].center(5) + "\t")
             else:
-                f.write("U" + "\t")
+                f.write("U".center(5) + "\t")
             for j in range(cols):
-                f.write(str(matrix[i][j]) + "\t")
+                f.write(str(matrix[i][j]).center(5) + "\t")
             f.write("\n")
         # Imprime a sequência 2
         f.write("\tU")
         for base in reversed(seq2):
-            f.write("\t" + base)
+            f.write("\t" + base.center(5))
         f.write("\n")
-
+        
 # Leitura das configurações do arquivo
 with open('input.txt', 'r') as file:
     seq1 = file.readline().strip()
